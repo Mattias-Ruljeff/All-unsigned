b18c01af40473042efbaf66cbf6b69050fd1401d
MattRul <mattias.ruljeff@gmail.com>
2020-12-08 12:37:14 +0100

    test

--- configs/database.js
+++ configs/database.js
@@ -3,15 +3,15 @@
 const mysql = require("mysql");
 require("dotenv").config();
 
-// const connection = mysql.createConnection({
-//   host: process.env.HOST,
-//   user: process.env.USER,
-//   password: process.env.PASSWORD,
-//   database: process.env.DATABASE,
-//   port: process.env.DB_PORT,
-// });
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.DB_PORT,
+});
 
-const connection = mysql.createConnection(process.env.JAWSDB_URL);
+// const connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 connection.connect();
 
--- controllers/index.js
+++ controllers/index.js
@@ -1,51 +1,45 @@
-'use strict'
+"use strict";
 
 // Connect to mysql
-const connection = require('../configs/database')
+const connection = require("../configs/database");
 
-const indexController = {}
+const indexController = {};
 
 indexController.index = async (req, res) => {
-    try {
-        
-        await connection.query(
-            'SELECT * FROM band',
-            (error, result, fields) => {
-                if (error) {
-                    throw error;
-                }
+  try {
+    await connection.query("SELECT * FROM band", (error, result, fields) => {
+      if (error) {
+        throw error;
+      }
 
-                res.status(200)
-                res.json({
-                    msg: 'I found it :).',
-                    result
-                })
-            }
-        )
-
-    } catch (error) {
-        console.log(error)
-        res.status(400).json({ msg: 'Error: ' + error })
-    }
-}
+      res.status(200);
+      // res.json({
+      //     msg: 'I found it :).',
+      //     result
+      // })
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ msg: "Error: " + error });
+  }
+};
 
 indexController.create = async (req, res) => {
-    try {
-        console.log('Hello World! POST')
+  try {
+    console.log("Hello World! POST");
 
-        // connection.query('INSERT INTO band(name) VALUES("")')
-        connection.query(`INSERT INTO band(name) VALUES("${req.body.name}")`)
+    // connection.query('INSERT INTO band(name) VALUES("")')
+    connection.query(`INSERT INTO band(name) VALUES("${req.body.name}")`);
 
-        res.status(200)
-        res.json({
-            msg: 'Query was added.'
-        })
-  
-    } catch (error) {
-        console.log(error)
-        res.status(400).json({ msg: 'Error: ' + error })
-    }
-}
+    res.status(200);
+    res.json({
+      msg: "Query was added.",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ msg: "Error: " + error });
+  }
+};
 
 // Exports
-module.exports = indexController
+module.exports = indexController;
