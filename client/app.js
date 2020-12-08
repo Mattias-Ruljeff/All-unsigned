ad99d62452be41dac52c708fbd5961a69f1409a1
Oscar Elf Svensson <os222di@student.lnu.se>
2020-12-08 10:53:36 +0100

    vkcx

--- client/app.js
+++ client/app.js
@@ -1,5 +1,13 @@
 'use strict'
 
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
+
 async function fetchDatabase(data) {
     
     const div = document.querySelector("#app")
