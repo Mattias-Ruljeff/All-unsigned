5fb3394ac06912faa60590ed78f231e0616796f6
Oscar Elf Svensson <os222di@student.lnu.se>
2020-12-08 11:35:26 +0100

    testing heroku with react

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const cors = require('cors')
 const logger = require('morgan')
 const path = require('path')
 
+require('dotenv').config()
+
 // Create express application
 const app = express()
 
@@ -16,12 +18,15 @@ app.use(express.json()) // Parses JSON
 // Routes
 app.use('/', require('./routes'))
 
-// Set static folder.
-app.use(express.static('client'))
+// Serve static assets if in production.
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder.
+  app.use(express.static('client/build'))
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
-})
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 // Start listening
 const port = process.env.PORT || 5000
