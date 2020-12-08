34c3ce5bd3e05bd2c889b3540132e1799e125fe7
Oscar Elf Svensson <os222di@student.lnu.se>
2020-12-08 10:39:13 +0100

    daedfx

--- configs/database.js
+++ configs/database.js
@@ -1,14 +1,14 @@
 'use strict'
 
 const mysql = require('mysql')
-// require('dotenv').config()
+require('dotenv').config()
 
 const connection = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "root",
-	database: "all-unsigned",
-	port: 8889,
+	host: process.env.HOST,
+	user: process.env.USER,
+	password: process.env.PASSWORD,
+	database: process.env.DATABASE,
+	port: process.env.DB_PORT,
 })
 
 connection.connect()
