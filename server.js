a966b08c8e3fad7d562a96186488f20f83eadcf5
MattRul <mattias.ruljeff@gmail.com>
2020-12-08 12:31:12 +0100

    test...

--- server.js
+++ server.js
@@ -22,10 +22,10 @@ app.use("/", require("./routes"));
 // if (process.env.NODE_ENV === 'production') {
 // Set static folder.
 // app.use(express.static("client/build"));
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build"));
+  res.sendFile(path.join(__dirname, "client/build"));
 });
 // }
 
