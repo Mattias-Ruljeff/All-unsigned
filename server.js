d8f9708c3de7af7dce77512f7a976c55512facb7
Oscar Elf Svensson <os222di@student.lnu.se>
2020-12-08 11:48:23 +0100

    sdfs

--- server.js
+++ server.js
@@ -19,14 +19,14 @@ app.use(express.json()) // Parses JSON
 app.use('/', require('./routes'))
 
 // Serve static assets if in production.
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
   // Set static folder.
   app.use(express.static('client/build'))
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
-}
+// }
 
 // Start listening
 const port = process.env.PORT || 5000
